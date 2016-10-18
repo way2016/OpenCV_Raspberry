385c8726ee14485e14d73770b6bd8a766be9eda7
ChuanWei Lai <chuanway@ChuanWeide-MBP.Home>
2016-10-18 23:38:27 +0800

    add 3 learning programs

---
+++ learn_03/CMakeFiles/Gray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gray.dir/Gray.cpp.o"
+  "Gray.pdb"
+  "Gray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
